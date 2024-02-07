6f83d0c1be6bd75748f82ca50b9bf95534579dfa
Prophymate <92168391+Prophymate@users.noreply.github.com>
2024-02-07 16:51:21 +0100

    Mine Fonctionnelle, grenade aussi

--- Assets/Scripts/Mine.cs
+++ Assets/Scripts/Mine.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class Grenade : MonoBehaviour
+public class Mine : MonoBehaviour
 {
-    public Rigidbody rb;
-    [SerializeField] private LayerMask _layer;
     [SerializeField] private Explosion _explosion;
     [SerializeField] private float forceExplosion;
     [SerializeField] private float radiusExplosion;
+    [SerializeField] private float _timerExplosion;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.GetComponent<PlayerMovementAndCameraFPS>() != null)
+        {
+            StartCoroutine(CoroutineIsPressed());
+        }
+    }
 
     private void OnDestroy()
     {
         Explosion explosion = Instantiate(_explosion, transform.position, Quaternion.identity);
         explosion._forceExplosion = forceExplosion;
         explosion._radiusExplosion = radiusExplosion;
+    }
+
+    private IEnumerator CoroutineIsPressed()
+    {
+        yield return new WaitForSeconds(_timerExplosion);
+        Destroy(this.gameObject);
     }
 }
--- Assets/Scripts/Player/PlayerThrowGrenade.cs
+++ Assets/Scripts/Player/PlayerThrowGrenade.cs
@@ -6,6 +6,7 @@ public class PlayerThr : MonoBehaviour
 {
     [SerializeField] private Camera _Camera;
     [SerializeField] private Grenade _grenadePrefab;
+    [SerializeField] private Mine _mine;
     [SerializeField] private float _throwForce;
     [SerializeField] private float _timerExplosion;
 
@@ -16,6 +17,10 @@ public class PlayerThr : MonoBehaviour
             Grenade grenade = Instantiate(_grenadePrefab, _Camera.transform.position + transform.forward, Quaternion.identity);
             grenade.rb.AddForce(_Camera.transform.forward * _throwForce, ForceMode.Impulse);
             Destroy(grenade.gameObject, _timerExplosion);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Mine mine = Instantiate(_mine, _Camera.transform.position + (transform.forward * 2.5f), Quaternion.identity);
         }
     }
 }
