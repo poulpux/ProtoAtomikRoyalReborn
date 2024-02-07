23624a89070c25c1d4c72f14c20f21996de26376
Prophymate <92168391+Prophymate@users.noreply.github.com>
2024-02-07 14:50:25 +0100

    ajout player et plane

---
+++ Assets/Scripts/Player/JumpFPS.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class JumpFPS : MonoBehaviour
+{
+    [Header("======Jump======")]
+    [Space(10)]
+    [SerializeField] private float jumpPow = 5f;
+    Rigidbody rb;
+    [TextArea]
+    public string AboutTool = "Attention : Mettre la layer (Player) au Player. \nFonction de saut opti, detection du sol grace a un raycasting aux pied du personnage. ";
+    float timerJump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        allTimer();
+        if (Input.GetButtonDown("Jump") && CanJump() == true)
+            Jump();
+    }
+    private void allTimer()
+    {
+        timerJump += Time.deltaTime;
+    }
+    private bool CanJump()
+    {
+        return testToucheGround() == true && timerJump > 0.1f ? true : false;
+    }
+    private void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpPow, ForceMode.Impulse);
+        timerJump = 0;
+    }
+    private bool testToucheGround()
+    {
+        Ray ray = new Ray(transform.position, -transform.up);
+        return Physics.Raycast(ray, out RaycastHit hit, transform.localScale.y + 0.2f, ~(1 << LayerMask.NameToLayer("Player"))) ? true : false;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovementAndCameraFPS.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovementAndCameraFPS : MonoBehaviour
+{
+    [Header("======Player======")]
+    [Space(10)]
+    [SerializeField] float spdMoovement = 5f;
+    private Rigidbody rb;
+    [Header("======Camera======")]
+    [Space(10)]
+    [SerializeField] float mouseSensiX = 5f;
+    [SerializeField] float mouseSensiY = 5f;
+    [SerializeField] Camera cam;
+    [TextArea]
+    public string AboutTool = "C'est un outil qui Garenti des mouvements pour FPS cleans dont:\r\n-Horizontal / Vertical pour le player.\r\n-Rotation de cam qui marche et la camera qui se bloque.";
+    float rotationX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //On rend invisible la souris
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        CamRotation();
+        ZQSDMouvement();
+    }
+    //Will Make all rotation of cam
+    private void CamRotation()
+    {
+        CamRotationX();
+        CamRotationY();
+    }
+    //On rotate la souris et on la bloque en vertical
+    private void CamRotationX()
+    {
+        rotationX -= Input.GetAxis("Mouse Y") * mouseSensiY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+        cam.transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+    }
+    //Fait rotater le joueur sur l'horizontale
+    private void CamRotationY()
+    {
+        transform.Rotate(0f, Input.GetAxis("Mouse X") * mouseSensiX, 0f);
+    }
+    private void ZQSDMouvement()
+    {
+        Vector2 dir = direction();
+        dir.Normalize();
+        rb.velocity = transform.localRotation * new Vector3(dir.x * spdMoovement, rb.velocity.y, dir.y * spdMoovement);
+    }
+    private Vector2 direction()
+    {
+        return new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+}
