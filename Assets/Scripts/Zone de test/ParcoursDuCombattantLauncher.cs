3449bbdad3c9dcf55118af0fbdf385145ff78f48
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-02-09 16:53:50 +0100

    Tout est Posé

--- Assets/Scripts/Zone de test/ParcoursDuCombattantLauncher.cs
+++ Assets/Scripts/Zone de test/ParcoursDuCombattantLauncher.cs
@@ -14,9 +14,12 @@ public class ParcoursDuCombattantLauncher : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI text, textTimer;
     private bool isDoingParcours, compteARebours;
+    [SerializeField]
+    private GameObject Confeti;
     float timer;
     void Start()
     {
+        Debug.Log(PlayerPrefs.GetFloat("BestScore").ToString("F2"));
         InstantiateAll();
     }
 
@@ -25,11 +28,10 @@ public class ParcoursDuCombattantLauncher : MonoBehaviour
         timer += Time.deltaTime;
         if (CanActivate())
             SetTextHelp();
-        else if (!isDoingParcours)
+        else if (!isDoingParcours && timer > 3f)
             text.text = "";
 
-
-        if (CanActivate() && Input.GetKeyDown(KeyCode.E)) 
+        if (Vector3.Distance(transform.position, player.transform.position) < 2f && Input.GetKeyDown(KeyCode.E)) 
             LauchParcours();
         if (compteARebours)
             DuringCompteARebours();
@@ -39,10 +41,15 @@ public class ParcoursDuCombattantLauncher : MonoBehaviour
 
     private void InstantiateAll()
     {
+        timer = 0f;
+        if (PlayerPrefs.GetFloat("BestScore") == 0f)
+            PlayerPrefs.SetFloat("BestScore", 99f);
+        //PlayerPrefs.SetFloat("BestScore", 99f);
+        isDoingParcours = false;
         foreach (var item in allColliders)
             item.SetActive(false);
 
-        textTimer.text = "";
+        textTimer.text = "Record : "+ PlayerPrefs.GetFloat("BestScore").ToString("F2");
         text.text = "";
         triggerFinishZone.gameObject.SetActive(false);
     }
@@ -95,6 +102,26 @@ public class ParcoursDuCombattantLauncher : MonoBehaviour
 
     public void LevelFinish()
     {
-        Debug.Log("Arrivé");
+        float timerSafe = timer;
+        InstantiateAll();
+        if (timerSafe < PlayerPrefs.GetFloat("BestScore"))
+        {
+            PlayerPrefs.SetFloat("BestScore", timerSafe);
+            StartCoroutine(NouveauScore(true));
+        }
+        else
+            StartCoroutine(NouveauScore(false, timerSafe));
+
+    }
+
+    private IEnumerator NouveauScore(bool hightscore, float timerSave = 0f)
+    {
+        Debug.Log("passe");
+        text.text = hightscore ? "NEW HIGHTSCORE !!! : " + PlayerPrefs.GetFloat("BestScore").ToString("F2") : "Score : " + timerSave.ToString("F2");
+        text.fontSize = 80f;
+
+        yield return new WaitForSeconds(3f);
+
+        text.text = "";
     }
 }
