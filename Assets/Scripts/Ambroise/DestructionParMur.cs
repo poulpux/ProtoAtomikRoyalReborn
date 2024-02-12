8b9af25f65b62070114e6ff235441f72d2334c3e
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-02-12 15:08:11 +0100

    TestAvantLaConnerie

--- Assets/Scripts/Ambroise/DestructionParMur.cs
+++ Assets/Scripts/Ambroise/DestructionParMur.cs
@@ -30,9 +30,12 @@ public class DestructionParMur : MonoBehaviour
     {
         if(childs.Count == 0)
             Destroy(gameObject, 15f);
-        //if(childs.Count < nbChild / 2 && rb.isKinematic)
-        //    rb.isKinematic = false;
-        
+        if (childs.Count < nbChild / 2 && rb.isKinematic)
+        {
+            Debug.Log("Gravity");
+            rb.isKinematic = false;
+        }
+
 
         foreach (var item in childs)
         {
